765468a58dafee8f4897184e7ac3690e20d8244d
kendavidn-tgc <166381472+kendavidn-tgc@users.noreply.github.com>
2024-04-11 15:52:37 +0100

    added some files

--- main.R
+++ main.R
@@ -2,8 +2,10 @@ library(tidyverse)
 library(here)
 
 tv_hours_tabulation <- gss_cat %>% 
-  filter(age < 31) %>% 
+  filter(age < 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
+
+# Noticed that divorced people seem to watch more TV
 
 write_csv(tv_hours_tabulation, here("TV_hours_by_Marital.csv"))
