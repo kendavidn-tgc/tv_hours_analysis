2f195a4d612fb8ed2ccbc334ad097a48a8fb8ae8
kendavidn-tgc <166381472+kendavidn-tgc@users.noreply.github.com>
2024-04-11 15:46:34 +0100

    Update main.R

--- main.R
+++ main.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(here)
 
 tv_hours_tabulation <- gss_cat %>% 
-  filter(age < 30) %>% 
+  filter(age < 31) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = mean(tvhours, na.rm = T))
 
